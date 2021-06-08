a623d4fbf529e8ed313f4edbbe6ea04099db410b
jhlee <gkgk545@gmail.com>
2021-06-08 23:02:00 +0900

    load fundermental data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { mealTable: [], isLoading: true };
+
+  getMealTable = async () => {
+    const mealTable =
+      await axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=d3d58b2620354978825b4f3bb4128595&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7731063
+    `);
+    this.setState({ mealTable: mealTable.request.response, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMealTable();
+  }
+
+  render() {
+    const { isLoading, mealTable } = this.state;
+    return <div>{isLoading ? "Loading" : console.log(mealTable)}</div>;
+  }
 }
 
 export default App;
